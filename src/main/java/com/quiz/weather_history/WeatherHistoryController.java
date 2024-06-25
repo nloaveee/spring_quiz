4edba1f14b5ddce7a7fbed373009004b44ea0b0a
hyeonheee <nloavee@gmail.com>
2024-06-25 09:03:03 +0900

    lesson05_quiz04까지

--- src/main/java/com/quiz/weather_history/WeatherHistoryController.java
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -1,5 +1,24 @@
 package com.quiz.weather_history;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/weather_history")
 public class WeatherHistoryController {
 
+	// http://localhost:8080/weather_history/weather-list-view
+	@GetMapping("weather-list-view")
+	public String weatherListView() {
+
+		return "weather_history/weatherList";
+	}
+
+	// http://localhost:8080/weather_history/add-weather-view
+	@GetMapping("add-weather-view")
+	public String addWeatherView() {
+
+		return "weather_history/addWeather";
+	}
 }
