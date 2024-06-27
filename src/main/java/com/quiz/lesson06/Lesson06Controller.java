4904dba197f963a44b5940a5960363c901f4cf93
hyeonheee <nloavee@gmail.com>
2024-06-27 11:38:51 +0900

    lesson06_quiz01까지

--- src/main/java/com/quiz/lesson06/Lesson06Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Controller.java
@@ -1,14 +1,25 @@
 package com.quiz.lesson06;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.quiz.lesson06.bo.BookmarkBO;
+import com.quiz.lesson06.domain.Bookmark;
+
 @Controller
 @RequestMapping("/lesson06")
 public class Lesson06Controller {
+
+	@Autowired
+	private BookmarkBO bookmarkBO;
 
 	// 즐겨 찾기 추가 화면
 	// http://localhost:8080/lesson06/add-bookmark-view
@@ -20,7 +31,9 @@ public class Lesson06Controller {
 	// AJAX가 요청하는 요청
 	@ResponseBody
 	@PostMapping("/add-bookmark")
-	public String addBookmark() {
+	public String addBookmark(@ModelAttribute Bookmark bookmark) {
+
+		bookmarkBO.addBookmark(bookmark);
 
 		return "성공";
 	}
@@ -28,7 +41,12 @@ public class Lesson06Controller {
 	// 결과 화면 (즐겨 찾기 목록)
 	// http://localhost:8080/lesson06/list-bookmark-view
 	@GetMapping("/list-bookmark-view")
-	public String listBookmarkView() {
+	public String listBookmarkView(@ModelAttribute Bookmark bookmark, Model model) {
+
+		List<Bookmark> bookmarkList = bookmarkBO.getBookmarkList();
+
+		model.addAttribute("bookmarkList", bookmarkList);
+
 		return "lesson06/listBookmark";
 	}
 
---
+++ src/main/java/com/quiz/lesson06/bo/BookmarkBO.java
@@ -0,0 +1,29 @@
+package com.quiz.lesson06.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson06.domain.Bookmark;
+import com.quiz.lesson06.mapper.BookmarkMapper;
+
+@Service
+public class BookmarkBO {
+
+	@Autowired
+	private BookmarkMapper bookmarkMapper;
+
+	// input: x
+	// ouput: List<Bookmark>
+	public List<Bookmark> getBookmarkList() {
+		return bookmarkMapper.selectBookmarkList();
+	}
+
+	// input: Bookmark
+	// output: x
+	public void addBookmark(Bookmark bookmark) {
+		bookmarkMapper.insertBookmark(bookmark);
+
+	}
+}
