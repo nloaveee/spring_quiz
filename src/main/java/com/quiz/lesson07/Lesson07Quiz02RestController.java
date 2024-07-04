d2aa0c8239a00351997a6f4c15bc69b390a49506
hyeonheee <nloavee@gmail.com>
2024-07-04 12:00:20 +0900

    lesson07_quiz02까지

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -27,11 +27,40 @@ public class Lesson07Quiz02RestController {
 	}
 
 	// 2. parameter 조건 조회 (/lesson07/quiz02/2)
+	// http://localhost:8080/lesson07/quiz02/2?companyId=1
 	// Request Parameter로 전달 받은 company id로 해당하는 회사의 공고를 조회
 	@GetMapping("/2")
 	public List<RecruitEntity> select2(@RequestParam("companyId") int companyId) {
 
-		return recruitRepository.findBycompanyId(1);
+		return recruitRepository.findBycompanyId(companyId);
+	}
+
+	// 3. 복합 조건 조회 (/lesson07/quiz02/3)
+	// 웹 back-end 개발자이고 정규직인 공고 조회
+	@GetMapping("/3")
+	public List<RecruitEntity> select3() {
+		return recruitRepository.findByPositionAndType("웹 back-end 개발자", "정규직");
+	}
+
+	// 4. 복합 조건 조회(/lesson07/quiz02/4)
+	// 정규직 이거나 연봉이 9000이상인 공고 조회
+	@GetMapping("/4")
+	public List<RecruitEntity> select4() {
+		return recruitRepository.findByTypeOrSalaryGreaterThanEqual("정규직", 9000);
+	}
+
+	// 5. 정렬 제한 조건 (/lesson07/quiz02/5)
+	// 계약직 목록을 연봉 기준으로 내림차순 정렬해서 3개만 조회
+	@GetMapping("/5")
+	public List<RecruitEntity> select5() {
+		return recruitRepository.findTop3ByTypeOrderBySalaryDesc("계약직");
+	}
+
+	// 6. 범위 조회 (/lesson07/quiz02/6)
+	// 성남시 분당구가 지역인 연봉 7000 이상 8500 이하인 공고 조회
+	@GetMapping("/6")
+	public List<RecruitEntity> select6() {
+		return recruitRepository.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
 	}
 
 }
--- src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
@@ -11,6 +11,19 @@ public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer>
 	// 1
 	public List<RecruitEntity> findById(int id);
 
+	// 2
 	public List<RecruitEntity> findBycompanyId(int id);
+
+	// 3
+	public List<RecruitEntity> findByPositionAndType(String position, String type);
+
+	// 4
+	public List<RecruitEntity> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
+
+	// 5
+	public List<RecruitEntity> findTop3ByTypeOrderBySalaryDesc(String type);
+
+	// 6
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int startSalary, int endSalary);
 
 }
